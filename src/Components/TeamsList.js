5c6c1bb7c2c054d0785b6bb85e5154bd4adf5c5c
Jason Ackley <Sparkyack@gmail.com>
2022-11-12 13:47:33 -0600

    Main Push

---
+++ src/Components/TeamsList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Team } from "./Team";
+import { teamsApi } from "../REST/TeamsApi";
+import "./styles.css";
+
+export class TeamsList extends React.Component {
+  state = {
+    teams: [],
+  };
+
+  componentDidMount() {
+    this.fetchTeams();
+  }
+
+  fetchTeams = async () => {
+    const teams = await teamsApi.get();
+    console.log("Teams list", teams);
+    this.setState({ teams });
+  };
+
+  updateTeam = async (updatedTeam) => {
+    await teamsApi.put(updatedTeam);
+    this.fetchTeams();
+  };
+
+  render() {
+    return (
+      <div className="table">
+        {this.state.teams.map((team) => (
+          <Team team={team} key={team._id} updateTeam={this.updateTeam} />
+        ))}
+      </div>
+    );
+  }
+}
